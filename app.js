0d5572e7fcac40c011cff40b990cde31b05415cd
your git username <your git email>
2020-12-23 10:37:52 +0900

    node_kadai1

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+const route = require('./routes/index.js');
 
 app.set('view engine', 'ejs');
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.use('/', require('./routes/index.js'));
+app.use('/', route);
 
 app.listen(3000);
